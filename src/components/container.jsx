2a40cf2a3b21fe08b0e25ca2cdd93dc8a1fd788b
Erudite098 <macalde.kerzania1029@gmail.com>
2025-08-23 20:57:51 +0800

    added necessary compoenents

---
+++ src/components/container.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function Container({ children, style, ...props }) {
+  return (
+    <View style={[styles.container, style]} {...props}>
+      {children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',    
+    alignItems: 'center',         
+    justifyContent: 'center',      
+    padding: 20,  
+    // paddingHorizontal: 25,
+
+    position: 'relative',          
+  },
+});
---
+++ src/data/carousel-data.js
@@ -0,0 +1,23 @@
+export const carouselData = [
+  {
+    id: 1,
+    title: "Matuto ng Baybayin",
+    description: "Explore breathtaking landscapes and serene environments.",
+    backgroundImage: require("../assets/bg1.png"),
+    screen: "LearnScreen",
+  },
+  {
+    id: 2,
+    title: "Merchs",
+    description: "Join us for thrilling adventures and unforgettable experiences.",
+    backgroundImage: require("../assets/bg2.jpg"),
+    screen: "MerchScreen",
+  },
+  {
+    id: 3,
+    title: "Baybayin Bill",
+    description: "Immerse yourself in diverse cultures and rich traditions.",
+    backgroundImage: require("../assets/bg1.png"),
+    screen: "BillScreen",
+  },
+];
